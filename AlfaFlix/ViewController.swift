1eaa5dd61d2ca60ee70175124599033b6e7138ae
Yoga <panji.pangestu@ist.id>
2025-08-07 21:28:18 +0700

    Initial Commit

---
+++ AlfaFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlfaFlix
+//
+//  Created by Yoga on 07/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
