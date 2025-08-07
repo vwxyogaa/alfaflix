2f970feaf70228b6eaa4f3a0fd0187127ceb2168
Yoga <panji.pangestu@ist.id>
2025-08-08 00:43:23 +0700

    configure main screen

--- AlfaFlix/Application/SceneDelegate.swift
+++ AlfaFlix/Application/SceneDelegate.swift
@@ -11,6 +11,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let mainViewController = UINavigationController(rootViewController: MainScreenViewController())
+        window.rootViewController = mainViewController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 }
